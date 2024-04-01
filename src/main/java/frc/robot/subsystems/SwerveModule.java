67c62a112acde27c6aa45b42cdaba9608340aa06
carolinemerkle <caroline.merkle@credohigh.org>
2024-03-31 20:19:29 -0700

    ...

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -1,5 +1,32 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+
 public class SwerveModule {
+
+    private TalonFX driveMotor;
+    private TalonFX steerMotor;
+    private CANcoder absoluteEncoder;
+
+public SwerveModule(int driveMotorCANID, int steerMotorCANID, int cancoderCANID){
+
+    driveMotor = new TalonFX(driveMotorCANID);
+    steerMotor = new TalonFX(steerMotorCANID);
+    absoluteEncoder = new CANcoder(cancoderCANID);
+
+    //reset devices to factory defaults 
+    driveMotor.getConfigurator().apply(new TalonFXConfiguration());
+    steerMotor.getConfigurator().apply(new TalonFXConfiguration());
+    absoluteEncoder.getConfigurator().apply(new CANcoderConfiguration());
+
+    //cancoder configuration
+
+
+    
+}
+
     
 }
